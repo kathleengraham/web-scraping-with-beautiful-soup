8d6814366e0766049ce1b923f0f5201f6cb2215c
Kathleen Graham <kathleeng77@outlook.com>
2019-08-13 12:26:43 -0500

    made .ipynb more efficient,added to scrape.py and index.html

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,3 +1,5 @@
+################################## SET UP ##################################
+
 # import dependencies
 import pandas as pd 
 from bs4 import BeautifulSoup as bs
@@ -12,17 +14,75 @@ def init_browser():
 # dictionary imported into Mongo
 mars_info = {}
 
-# NASA MARS NEWS
+
+############################## NASA MARS NEWS ##############################
 def scrape_mars_news():
     try: 
 
         # Initialize browser 
         browser = init_browser()
 
-        # more here
+        # store full url
+        news_url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
 
+        # visit site
+        browser.visit(news_url)
+
+        # parse through html
+        news_soup = bs(browser.html, 'html.parser')
+
+        # store title and headline
+        news_title = news_soup.find_all('div', class_='content_title')[0].text
+        news_p = news_soup.find_all('div', class_='article_teaser_body')[0].text
+        
+        # return results
         return mars_info
 
     finally:
-
+        # close browser
         browser.quit()
+
+
+################## JPL MARS SPACE IMAGES - FEATURED IMAGE ##################
+
+
+
+
+
+
+
+
+
+
+############################### MARS WEATHER ###############################
+
+
+
+
+
+
+
+
+
+
+################################ MARS FACTS ################################
+
+
+
+
+
+
+
+
+
+
+############################# MARS HEMISPHERES #############################
+
+
+
+
+
+
+
+
+
