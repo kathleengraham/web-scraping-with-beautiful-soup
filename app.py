3e3957b69373904066d1112977bb21d32ce4f10e
Kathleen Graham <kathleeng77@outlook.com>
2019-08-13 15:14:34 -0500

    added routes to app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+############################## SET UP ##############################
 # dependencies
 from flask import Flask, render_template, redirec
 import pymongo
@@ -13,26 +14,26 @@ client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client.mars_DB
 collection = db.mars_info
 
+############################## ROUTES ##############################
 # default route that renders index.html template
 @app.route("/")
 def index():
-
-# add index route here
-
-
-
-
-
+    # find data 
+    mars_info = db.mars_info.find_one()
+    # return template
+    return render_template("index.html", text=mars_info)
 
 # scrape route
 @app.route("/scrape")
 def scrape():
-
-# add scrape route here
-
-
-
-
+    mars_info = db.mars_info
+    mars_scraping = scrape_mars.scrape_mars_nasa_news()
+    mars_scraping = scrape_mars.scrape_mars_featured_image()
+    mars_scraping = scrape_mars.scrape_mars_weather_tweet()
+    mars_scraping = scrape_mars.scrape_mars_facts_table()
+    mars_scraping = scrape_mars.scrape_mars_hemispheres()
+    mars_info.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
